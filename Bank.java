886b9ca18d70648ac8ea5edf2a50d4cc4c51b946
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-27 15:46:58 -0800

    add new costumer to bank

--- Bank.java
+++ Bank.java
@@ -1,7 +1,14 @@
+import java.math.BigDecimal;
+
+import accounts.CheckingAccount;
+
 public class Bank {
     public static void main(String[] args) {
-        Costumer newCostumer = new Costumer("Angelic Wong", "Third Street, Vernon, British Columbia", "200-300-500");
-        
+        Costumer newCostumer = new Costumer("Angelic Wong", "Third Street, Vernon, British Columbia", "200-300-500", "C0001");
+
+        CheckingAccount checkingAccount = newCostumer.createCheckingAccount(0001, BigDecimal.valueOf(1000.00), BigDecimal.valueOf(100.00));
+
+        newCostumer.printCostumerInfo();
     }
     
 }
