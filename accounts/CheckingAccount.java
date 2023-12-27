82fab0a76ff4a7ed007eba482a71a8eead35a94e
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-27 15:25:44 -0800

    add properties to checking account to associate with costumer

--- accounts/CheckingAccount.java
+++ accounts/CheckingAccount.java
@@ -4,9 +4,13 @@ import java.math.BigDecimal;
 
 public class CheckingAccount extends Account {
     private BigDecimal minimumBalance; 
+    private BigDecimal initialBalance; 
+    private String costumerId; 
 
-    public CheckingAccount () {
-       this.minimumBalance = new BigDecimal(100.00); 
+    public CheckingAccount (String costumerId, BigDecimal initialBalance, BigDecimal minimumBalance) {
+       this.minimumBalance = minimumBalance;
+       this.initialBalance = initialBalance; 
+       this.costumerId = costumerId; 
     }
 
     public BigDecimal getMinimumBalance () {
