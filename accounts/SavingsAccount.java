905183df7563e33a40faee586071344a6be8fdb7
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-27 14:31:45 -0800

    add savings account class

---
+++ accounts/SavingsAccount.java
@@ -0,0 +1,29 @@
+package accounts;
+
+import java.math.BigDecimal;
+
+public class SavingsAccount extends Account {
+    final static BigDecimal INTEREST_RATE = new BigDecimal(0.6); 
+
+    public SavingsAccount () {
+
+    } 
+
+    public void addInterest() {
+        BigDecimal interest = this.getBalance().multiply(INTEREST_RATE); 
+        this.setBalance(this.getBalance().add(interest));
+        System.out.println("Interest added: $ " + interest); 
+    }
+
+    public void withdrawFromSavings (BigDecimal amount) {
+        if(amount.compareTo(BigDecimal.ZERO) > 0 && this.getBalance().compareTo(amount) >= 0) {
+            this.setBalance(this.getBalance().subtract(amount));
+            System.out.println("Sucessfully withdrawn: $ " + amount); 
+        } else {
+            System.out.println("Invalid amount or insuffient funds.");
+        }
+    }
+    
+     
+    
+}
