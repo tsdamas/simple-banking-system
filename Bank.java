0518ddf1c8688cc4a2ff0472939d366356bb98ff
Thais Santos Damasceno <tssdamasceno@gmail.com>
2023-12-27 15:12:46 -0800

    create new costumer

---
+++ Bank.java
@@ -0,0 +1,7 @@
+public class Bank {
+    public static void main(String[] args) {
+        Costumer newCostumer = new Costumer("Angelic Wong", "Third Street, Vernon, British Columbia", "200-300-500");
+        
+    }
+    
+}
